4d3b3bb092ae0fc4444bb2f4145cc8f7bf8a894d
mat sipahi <34083477+mat-sipahi@users.noreply.github.com>
2023-02-12 02:45:11 +0100

    Update build.sh

--- NNDisParser/build/build.sh
+++ NNDisParser/build/build.sh
@@ -1,2 +1,2 @@
-path=/home/yunan/workspace/discourseParser/
-cmake .. -DEIGEN3_DIR=${path}eigen -DN3L_DIR=${path}N3LDG -DMKL=TRUE
+path=$(pwd)/../../
+cmake --clean-first .. -DEIGEN3_DIR=${path}eigen -DN3L_DIR=${path}N3LDG -DMKL=TRUE -DCMAKE_BUILD_TYPE=Debug
